9436e13cef57e7b7075ff147d63737d7a53623e0
Bangare123 <adityabangare123@gmail.com>
2025-04-28 22:54:52 +0530

    First commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import logo from "../assets/Main_logo.jpg";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={logo} className="mb-5 w-48 h-28" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry`s standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91 9527459675</li>
+            <li>bangareaditya321@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ forever.com - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
